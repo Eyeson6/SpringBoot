cbdeaa82fac49d47ddcb13a59455c97b18fb3961
Blues <13482959@qq.com>
2017-12-01 10:48:49 +0800

    集成redis

---
+++ src/main/java/com/HelloWorldController.java
@@ -0,0 +1,40 @@
+package com;
+
+import com.mapper.UserMapper;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by blues on 2017/12/1.
+ */
+@RestController
+public class HelloWorldController {
+
+    private  final org.slf4j.Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("/getUser")
+    public Map<String,Object> getUser() {
+        System.out.println("11");
+        logger.info("日志不在这里！");
+        List<Integer> list =  userMapper.getAll();
+        System.out.println(list.size()+list.size());
+        Map<String,Object> resultModel = new HashMap<String,Object>();
+        resultModel.put("test",list);
+        redisTemplate.opsForValue().increment("zj",2);
+        System.out.println(redisTemplate.opsForValue().get("zj"));
+        return resultModel;
+    }
+}
